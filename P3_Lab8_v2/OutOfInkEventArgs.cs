63949ee96674df1f0d0451f2b9633aa2b518de4d
Daniel <daniel.lipowczan@gmail.com>
2020-01-28 12:40:48 +0100

    commit

---
+++ P3_Lab8_v2/OutOfInkEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P3_Lab8_v2
+
+{
+    public class OutOfInkEventArgs : EventArgs
+    {
+        public OutOfInkEventArgs(string color)
+        {
+            Color = color;
+        }
+        public string Color { get; set; }
+    }
+}
---
+++ P3_Lab8_v2/OutOfPaperEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3_Lab8_v2
+{
+    public class OutOfPaperEventArgs : EventArgs
+    {
+        public OutOfPaperEventArgs(int PNumber)
+        {
+            this.PNumber = PNumber;
+        }
+
+        public int PNumber { get; set; }
+    }
+}
